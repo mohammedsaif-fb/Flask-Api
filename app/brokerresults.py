a910a51d759b4e8115aa35b185f8369214ca79de
Mohammed Saifullah <mohammed.saif@fishbonesolutions.co.uk>
2023-04-13 11:35:12 +0000

    order

--- app/brokerresults.py
+++ app/brokerresults.py
@@ -27,7 +27,7 @@ def get_broker_logs():
 # ...
     cursor2 = connection2.cursor()
 
-    query = ("SELECT timestamp as timestamp, topic as topic, message as message, id as id from broker_messages")
+    query = ("SELECT timestamp as timestamp, topic as topic, message as message, id as id from broker_messages ORDER BY timestamp desc")
 
     cursor2.execute(query)
 
